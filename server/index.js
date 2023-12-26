e0b710d0d92512579e768e4cefe0dd8a8310f459
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-12-26 10:05:49 +0530

    feat: create mongoDb connection successsfully

--- server/index.js
+++ server/index.js
@@ -6,10 +6,21 @@ import { getApiHealth } from './controllers/helth.js';
 
 
 const app = express();
-app.use (express.json());
+app.use(express.json());
 
 
-
+try{
+    const connectionDb = async () => {
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
+           if (conn) {
+              console.log('MongoDB connected successfully !');
+           }
+        };
+        connectionDb()
+}
+catch(err){
+   console.log(err.message)
+}
 
 // helth API
 app.get('/api/health',getApiHealth );
